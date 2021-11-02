b1155fa62594786ec04005971524f701c0afaf9e
TheGubbys <elmevej18denni@live.dk>
2021-11-02 15:05:43 +0100

    småting

--- index.php
+++ index.php
@@ -50,10 +50,10 @@
                 <div class="mt-4">
                     <div class="row p-3">
                         <div class="col-md-9 col-12 border-bottom">
-                            <input type="text" class="border-0 w-100 b-text" placeholder="Søg på navn eller addresse">
+                            <input type="text" class="border-0 w-100 b-text"  placeholder="Søg på navn eller addresse">
                         </div>
                         <div class="col-md-3 col-12 text-center">
-                            <a class="btn cream d-flex mt-2 mt-md-0 justify-content-center" href="#input">
+                            <a class="btn cream d-flex mt-2 mt-md-0 justify-content-center" href="./search.php">
                                 <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
                                     <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"></path>
                                 </svg>
@@ -68,7 +68,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text left cream">Max Salgspris</span>
                             </div>
-                            <input type="number" class="form-control center1 border" aria-label="">
+                            <input type="number" min="0" class="form-control center1" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
                         </div>
                     </div>
                     <div class="col-12 p-2">
@@ -76,7 +76,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text left cream">Min. Areal</span>
                             </div>
-                            <input type="number" class="form-control center1 border" aria-label="">
+                            <input type="number" min="0" class="form-control center1" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
                             <div class="input-group-append">
                                 <span class="input-group-text right cream">m<sup>2</sup></span>
                             </div>
@@ -87,7 +87,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text left cream">Max. Areal</span>
                             </div>
-                            <input type="number" class="form-control center1 border" aria-label="">
+                            <input type="number" min="0" class="form-control center1" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
                             <div class="input-group-append">
                                 <span class="input-group-text right cream">m<sup>2</sup></span>
                             </div>
@@ -102,7 +102,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text left cream">Max Salgspris</span>
                             </div>
-                            <input type="number" class="form-control center" aria-label="">
+                            <input type="number" min="0" class="form-control center" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
                         </div>
                     </div>
                     <div class="col-6 col-xl-3 p-2">
@@ -110,7 +110,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text left cream">Min. Areal</span>
                             </div>
-                            <input type="number" class="form-control center" aria-label="">
+                            <input type="number" min="0" class="form-control center" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
                             <div class="input-group-append">
                                 <span class="input-group-text right cream">m<sup>2</sup></span>
                             </div>
@@ -121,7 +121,7 @@
                             <div class="input-group-prepend">
                                 <span class="input-group-text left cream">Max. Areal</span>
                             </div>
-                            <input type="number" class="form-control center" aria-label="">
+                            <input type="number" min="0" class="form-control center" oninput="this.value = !!this.value && Math.abs(this.value) >= 0 ? Math.abs(this.value) : null">
                             <div class="input-group-append">
                                 <span class="input-group-text right cream">m<sup>2</sup></span>
                             </div>
